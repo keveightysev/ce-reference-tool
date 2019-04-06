94fc9bc555cad906a947dcf5258968a0d92377b7
keveightysev <keveightysev@gmail.com>
2019-04-06 14:40:01 -0500

    Added login endpoint

--- users/route.js
+++ users/route.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcryptjs');
 
 const Users = require('./model.js');
 
+const generateToken = require('../auth/generateToken.js');
+
 router.post('/register', async (req, res) => {
   if (!req.body.first_name || !req.body.last_name) {
     res.status(406).json({ message: 'Full name required' });
@@ -25,6 +27,22 @@ router.post('/register', async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: 'Server error creating new user' });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  let { username, password } = req.body;
+  try {
+    const user = await Users.findBy({ username }).first();
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = generateToken(user);
+      res.status(200).json({ message: `Welcome ${user.first_name}!`, token });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Server error logging in' });
   }
 });
 
